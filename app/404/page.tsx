122c4cb81d2d1cc71e697fb82f5df4c0bdc2b9a2
SarthakSri98 <sarthaksri1998@gmail.com>
2025-03-06 23:15:57 +0530

    feat/reverted-splash

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+import React from 'react';
+
 export default function ErrorComponent() {
- 
   return (
     <div className="mt-16 px-4 text-center">
-     Error
+      Error
     </div>
   );
 }
