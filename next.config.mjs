4ace86c7be51577073b8c242b14176f5091c0ec2
SarthakSri98 <sarthaksri1998@gmail.com>
2025-03-06 22:06:59 +0530

    feat/next.config-fixe

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['picsum.photos', 'placehold.co', 'dodo-profile-audio.s3.ap-south-1.amazonaws.com']
-    }
+    },
+    reactStrictMode: true,
+    output: "standalone", // Supports dynamic pages
+    experimental: { appDir: true },
 };
 
 export default nextConfig;
