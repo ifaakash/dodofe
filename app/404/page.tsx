2d3366e436010c2a75d35acca4d2504b8706054f
SarthakSri98 <sarthaksri1998@gmail.com>
2025-03-06 23:44:58 +0530

    feat/refactor

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,7 +1,13 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function ErrorComponent() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="mt-16 px-4 text-center">
       Error
