b8ff6917644875482e0cff2a8e9e55ac071666dc
SarthakSri98 <sarthaksri1998@gmail.com>
2025-03-08 00:06:25 +0530

    feat/gitignore

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 
+export const dynamic = "force-dynamic"; // ✅ Forces dynamic rendering
+
 export default function ErrorComponent() {
   const [isClient, setIsClient] = useState(false);
 
