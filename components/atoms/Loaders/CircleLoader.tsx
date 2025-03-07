1df4cf07115b28298803481d438f06ee0625c59c
SarthakSri98 <sarthaksri1998@gmail.com>
2025-03-08 02:10:16 +0530

    feat/loader

--- components/atoms/Loaders/CircleLoader.tsx
+++ components/atoms/Loaders/CircleLoader.tsx
@@ -1,34 +1,32 @@
-"use client"; // Force client-only execution
-
+'use client'
 import React from 'react'
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-
-import useLoaderVisibility from 'hooks/useLoaderVisibility'
-
+import LoaderAnimation from 'public/animations/loader.json'
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import useLoaderVisibility from 'hooks/useLoaderVisibility'
+import dynamic from 'next/dynamic';
 
 const CircleLoader = () => {
     const { isVisible, showOverlay } = useLoaderVisibility();
-    const [animationData, setAnimationData] = useState(null);
 
-    useEffect(() => {
-        fetch('public/animations/loader.json')
-            .then((res) => res.json())
-            .then((data) => setAnimationData(data));
-    }, []);
-
-    if (!isVisible || !animationData) return null;
+    if (!isVisible) return null;
 
     return (
         showOverlay ?
             <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                <Lottie animationData={animationData} loop className="w-40 h-40" />
+                <Lottie
+                    animationData={LoaderAnimation}
+                    loop={true}
+                    className='w-40 h-40'
+                />
             </div> :
             <div className="flex items-center justify-center h-screen">
-                <Lottie animationData={animationData} loop className="w-40 h-40" />
+                <Lottie
+                    animationData={LoaderAnimation}
+                    loop={true}
+                    className='w-40 h-40'
+                />
             </div>
-    );
-};
+    )
+}
 
-export default CircleLoader;
+export default CircleLoader
