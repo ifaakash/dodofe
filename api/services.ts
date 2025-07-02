6fa091c2820ce65f84b53236480b34cc7460505b
ksatyarth2 <erkumar@protonmail.ch>
2025-07-02 23:22:35 +0530

    fix: add brand collab

--- api/services.ts
+++ api/services.ts
@@ -239,9 +239,7 @@ export const verifyMediaKit = async (instaId: string): Promise<any> =>
     Get<any>(API_CONSTANTS.verifyMediaKit + API_CONSTANTS.slash + instaId);
 
 export const updateMediaKitBrand = async (payload: any): Promise<any> =>
-    Patch<any>(API_CONSTANTS.updateMediaKitBrand, payload, {
-        "Content-Type": "multipart/form-data",
-    });
+    Patch<any>(API_CONSTANTS.updateMediaKitBrand, payload);
 
 export const deleteBrandCollaboration = async (payload: any): Promise<any> =>
     Delete<any>(API_CONSTANTS.deleteBrandCollaboration, payload);
--- components/molecules/mediaKitBlocks/BrandCollaboration.tsx
+++ components/molecules/mediaKitBlocks/BrandCollaboration.tsx
@@ -38,45 +38,85 @@ const BrandCollaboration = ({ brandData, setUpdateMediaKit, instaId, mode = 'edi
 
     return (
         <>
-            <div className={`p-[10px] bg-[#FDFBFF] rounded-xl flex flex-col gap-1`}>
-                <div className={`flex justify-end items-center ${mode === 'public' ? 'hidden' : ''}`}>
+            <div
+                className={`p-[10px] bg-[#FDFBFF] rounded-xl flex flex-col gap-1`}
+            >
+                <div
+                    className={`flex justify-end items-center ${
+                        mode === "public" ? "hidden" : ""
+                    }`}
+                >
                     {/* <Image className={`w-5 h-5 ${allBrandData?.isActive ? 'pointer-events-auto' : 'pointer-events-none'}`} src={DragIcon} alt="Drag" /> */}
                     <div className="pointer-events-auto">
-                        <Toggle checked={allBrandData?.isActive} onCheckedChange={handleToggle} />
+                        <Toggle
+                            checked={allBrandData?.isActive}
+                            onCheckedChange={handleToggle}
+                        />
                     </div>
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <div className={`flex justify-between items-center bg-gradient-to-r from-[#F2F1F3] to-[#FDFBFF] rounded-lg`}>
+                    <div
+                        className={`flex justify-between items-center bg-gradient-to-r from-[#F2F1F3] to-[#FDFBFF] rounded-lg`}
+                    >
                         <div className="text-[#9747FF] w-full px-2 py-1 flex gap-1 text-xs">
                             <div className="font-semibold"> Brand </div>
                             <div className=""> COLLABORATION </div>
                         </div>
-                        <Image src={BrandCollaborationIcon} alt="Gender Distribution" />
+                        <Image
+                            src={BrandCollaborationIcon}
+                            alt="Gender Distribution"
+                        />
                     </div>
 
-                    <div className={`flex gap-2 overflow-x-auto scrollbar-hide py-2 ${allBrandData?.isActive ? 'pointer-events-auto' : 'pointer-events-none'}`}>
-                        {
-                            allBrandData?.brands?.map((brand: any, index: number) => (
-                                <BrandCard key={index} brand={brand} mode={mode} instaId={instaId} />
-                            ))
-                        }
+                    <div
+                        className={`flex gap-2 overflow-x-auto scrollbar-hide py-2 ${
+                            allBrandData?.isActive
+                                ? "pointer-events-auto"
+                                : "pointer-events-none"
+                        }`}
+                    >
+                        {allBrandData?.brands?.map(
+                            (brand: any, index: number) => (
+                                <BrandCard
+                                    key={index}
+                                    brand={brand}
+                                    mode={mode}
+                                    instaId={instaId}
+                                />
+                            )
+                        )}
                     </div>
 
-                    <div className={`flex justify-center ${mode === 'public' ? 'hidden' : ''}`}>
-                        <div className="px-2 py-1 flex gap-1 items-center border border-[#E2E4E9] rounded-full cursor-pointer" onClick={() => setIsAddBrandModelOpen(true)}>
-                            <div className="text-[10px] font-semibold"> Add New </div>
+                    <div
+                        className={`flex justify-center ${
+                            mode === "public" ? "hidden" : ""
+                        }`}
+                    >
+                        <div
+                            className="px-2 py-1 flex gap-1 items-center border border-[#E2E4E9] rounded-full cursor-pointer"
+                            onClick={() => setIsAddBrandModelOpen(true)}
+                        >
+                            <div className="text-[10px] font-semibold">
+                                {" "}
+                                Add New{" "}
+                            </div>
                             <Plus size={16} className="text-brandPrimary" />
                         </div>
                     </div>
-
                 </div>
             </div>
-            {
-                isAddBrandModelOpen && <BrandModal setIsAddBrandModelOpen={setIsAddBrandModelOpen} setAllBrandData={setAllBrandData} allBrandData={allBrandData} variant="add" />
-            }
+            {isAddBrandModelOpen && (
+                <BrandModal
+                    setIsAddBrandModelOpen={setIsAddBrandModelOpen}
+                    setAllBrandData={setAllBrandData}
+                    allBrandData={allBrandData}
+                    variant="add"
+                    instaId={instaId}
+                />
+            )}
         </>
-    )
+    );
 }
 
 export default BrandCollaboration
