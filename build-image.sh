5a007b64da1ef1cb6d877621665aa5d3be26d3ed
ifaakash <aakashc.appregis@gmail.com>
2026-06-25 16:11:52 +0530

    feat(build-image): add push step to local registry after building

--- build-image.sh
+++ build-image.sh
@@ -4,3 +4,8 @@ echo "––– Building image for local registry –––"
 docker build -t localhost:5000/dodo-frontend -f Dockerfile .
 
 echo "––– BUILD COMPLETED –––"
+
+echo "––– Pushing image to local reistory –––"
+docker push localhost:5000/dodo-frontend
+
+echo "––– IMAGE PUSHED TO LOCAL REGISTRY –––"
